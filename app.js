2fda2092249325dfbad5efac7b2ddfbd68b36d86
Dmitriy Yakovlev <1d3ddd@gmail.com>
2022-05-20 18:16:56 +0300

    feat: add createPool connectionLimit  promise execute then catch

--- app.js
+++ app.js
@@ -1,43 +1,30 @@
-// Передаваемые в метод настройки конфигурации могут содержать ряд параметров. Наиболее используемые из них:
-
-// host: хост, на котором запущен сервер mysql. По умолчанию имеет значение "localhost"
-
-// port: номер порта, на котором запущен сервер mysql. По умолчанию имеет значение "3306"
-
-// user: пользователь MySQL, который используется для подключения
-
-// password: пароль для пользователя MySQL
-
-// database: имя базы данных, к которой идет подключение. Необязательный параметр. Если он не указан, то подключение идет в целом к северу
-
-// charset: кодировка для подключения, например, по умолчанию используется "UTF8_GENERAL_CI".
-
-// timezone: часовой пояс сервера MySQL. This is used to type cast server date/time values to JavaScript. По умолчанию имеет значение "local"
+// Пулы подключений позволяют уменьшить время, затраченное на подключение
+//  к серверу MySQL, благодаря повторному использованию подключений. 
+// Пул подключений создается с помощью функции createPool(). 
 
 const mysql = require("mysql2");
-  
-const connection = mysql.createConnection({
+const pool = mysql.createPool({
+  connectionLimit: 5,
   host: "localhost",
   user: "root",
-  database: "mysql",
+  database: "usersdb",
   password: "root",
-  port:"3307"
+  port: "3307"
+}).promise(); //пулы поддерживают работу с промисами:
 
 
-});
-// тестирование подключения
- connection.connect(function(err){
-    if (err) {
-      return console.error("Ошибка: " + err.message);
-    }
-    else{
-      console.log("Подключение к серверу MySQL успешно установлено");
-    }
- });
- // закрытие подключения
- connection.end(function(err) {
-  if (err) {
-    return console.log("Ошибка: " + err.message);
-  }
-  console.log("Подключение закрыто");
-});
+pool.execute("UPDATE users SET age=age+1 WHERE name=?", ["Stan"]) // изменение объектов
+    .then(result =>{ 
+      console.log(result[0]);
+      return pool.execute("SELECT * FROM users"); // получение объектов
+    })
+    .then(result =>{
+      console.log(result[0]);
+      pool.end();
+    })
+    .then(()=>{
+      console.log("пул закрыт");
+    })
+    .catch(function(err) {
+      console.log(err.message);
+    });
