8f0e0d19ffc30834fbddb32e96f0a5147c13f75b
Dmitriy Yakovlev <1d3ddd@gmail.com>
2022-05-20 19:02:12 +0300

    feat: add sequelize model methods

--- app.js
+++ app.js
@@ -1,79 +1,141 @@
-const mysql = require("mysql2");
-const express = require("express");
- 
-const app = express();
-const urlencodedParser = express.urlencoded({extended: false});
- 
-const pool = mysql.createPool({
-  connectionLimit: 5,
+// Sequelize - это ORM-библиотека для приложений на Node.js
+// При использовании Sequelize мы можем не писать SQL-запросы, а работать с данными как с обычными объектами. 
+// Причем Sequelize может работать с рядом СУБД - MySQL, Postgres, MariaDB, SQLite, MS SQL Server.
+// Для свойства dialect 
+// mysql
+// mariadb
+// sqlite
+// postgres
+// mssql
+// Ключевым компонентом в работе Sequelize с бд являются модели.
+// ! метода define()
+// const User = sequelize.define("user", {
+//   id: {
+//     type: Sequelize.INTEGER,
+//     autoIncrement: true,
+//     primaryKey: true,
+//     allowNull: false
+//   },
+//   name: {
+//     type: Sequelize.STRING,
+//     allowNull: false
+//   },
+//   age: {
+//     type: Sequelize.INTEGER,
+//     allowNull: false
+//   }
+// });
+//!  Второй способ определения модели:
+// class User extends Model {}
+// User.init({
+//   id: {
+//     type: Sequelize.INTEGER,
+//     autoIncrement: true,
+//     primaryKey: true,
+//     allowNull: false
+//   },
+//   name: {
+//     type: Sequelize.STRING,
+//     allowNull: false
+//   },
+//   age: {
+//     type: Sequelize.INTEGER,
+//     allowNull: false
+//   }
+// }, {
+//   sequelize,
+//   modelName: "user"
+// });
+
+//! примере модели
+const Sequelize = require("sequelize");
+const sequelize = new Sequelize("usersdb", "root", "root", {
+  dialect: "mysql",
   host: "localhost",
-  user: "root",
-  database: "usersdb",
-  password: "root",
-  port: "3307"
+  port:3307,
+  define: {
+    timestamps: false
+  }
 });
- 
-app.set("view engine", "hbs");
- 
-// получение списка пользователей
-app.get("/", function(req, res){
-    pool.query("SELECT * FROM users", function(err, data) {
-      if(err) return console.log(err);
-      res.render("index.hbs", {
-          users: data
-      });
-    });
+const User = sequelize.define("user", {
+  id: {
+    type: Sequelize.INTEGER,
+    autoIncrement: true,
+    primaryKey: true,
+    allowNull: false
+  },
+  name: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  age: {
+    type: Sequelize.INTEGER,
+    allowNull: false
+  }
 });
-// возвращаем форму для добавления данных
-app.get("/create", function(req, res){
-    res.render("create.hbs");
+sequelize.sync().then(result=>console.log(result))
+.catch(err=> console.log(err));
+
+
+//! Добавление данных
+
+User.create({
+  name: "Tom",
+  age: 35
+}).then(res=>{
+  console.log(res);
+}).catch(err=>console.log(err));
+
+User.create({
+  name: "Bob",
+  age: 31
+}).then(res=>{
+  const user = {id: res.id, name: res.name, age: res.age}
+  console.log(user);
+}).catch(err=>console.log(err));
+
+// !Получение данных
+User.findAll({raw:true}).then(users=>{
+  console.log(users);
+}).catch(err=>console.log(err));
+
+//! Простейшая фильтрация
+User.findAll({where:{name: "Tom"}, raw: true })
+.then(users=>{
+  console.log(users);
+}).catch(err=>console.log(err));
+
+// !Получение одного объекта findByPk() (получает объект по первичному ключу)
+User.findByPk(2)
+.then(user=>{
+    if(!user) return; // если пользователь не найден
+    console.log(user.name);
+}).catch(err=>console.log(err));
+
+//! Получение одного объекта findOne()
+
+User.findOne({where: {name: "Tom"}})
+.then(user=>{
+    if(!user) return;
+    console.log(user.name, user.age);
+}).catch(err=>console.log(err));
+
+//! Для обновления применяется метод update()
+
+User.update({ age: 36 }, {
+  where: {
+    name: "Bob"
+  }
+}).then((res) => {
+  console.log(res);
 });
-// получаем отправленные данные и добавляем их в БД 
-app.post("/create", urlencodedParser, function (req, res) {
-         
-    if(!req.body) return res.sendStatus(400);
-    const name = req.body.name;
-    const age = req.body.age;
-    pool.query("INSERT INTO users (name, age) VALUES (?,?)", [name, age], function(err, data) {
-      if(err) return console.log(err);
-      res.redirect("/");
-    });
-});
- 
-// получем id редактируемого пользователя, получаем его из бд и отправлям с формой редактирования
-app.get("/edit/:id", function(req, res){
-  const id = req.params.id;
-  pool.query("SELECT * FROM users WHERE id=?", [id], function(err, data) {
-    if(err) return console.log(err);
-     res.render("edit.hbs", {
-        user: data[0]
-    });
-  });
-});
-// получаем отредактированные данные и отправляем их в БД
-app.post("/edit", urlencodedParser, function (req, res) {
-         
-  if(!req.body) return res.sendStatus(400);
-  const name = req.body.name;
-  const age = req.body.age;
-  const id = req.body.id;
-   
-  pool.query("UPDATE users SET name=?, age=? WHERE id=?", [name, age, id], function(err, data) {
-    if(err) return console.log(err);
-    res.redirect("/");
-  });
-});
- 
-// получаем id удаляемого пользователя и удаляем его из бд
-app.post("/delete/:id", function(req, res){
-          
-  const id = req.params.id;
-  pool.query("DELETE FROM users WHERE id=?", [id], function(err, data) {
-    if(err) return console.log(err);
-    res.redirect("/");
-  });
-});
- 
-app.listen(3000, function(){
-  console.log("Сервер ожидает подключения...");
+
+//!Для удаления используется метод destroy()
+
+User.destroy({
+  where: {
+    name: "Bob"
+  }
+}).then((res) => {
+  console.log(res);
 });
